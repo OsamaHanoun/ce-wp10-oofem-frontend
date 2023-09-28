bb4c0334e882a1f084abf766841ef645f65c4096
Osama Hanoun <osamahanoun997@gmail.com>
2023-09-28 11:05:43 +0200

    feat: add helper axes

--- visualize.js
+++ visualize.js
@@ -1,74 +1,72 @@
-import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import drawFigure from './draw-figure.js';
-import drawConstraints from './draw-constraints.js';
-import drawForces from './draw-forces.js';
-import addAxises from './add-axises.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import drawFigure from "./draw-figure.js";
+import drawConstraints from "./draw-constraints.js";
+import drawForces from "./draw-forces.js";
+import { ViewHelper } from "./ViewHelper.js";
 
-// Parse uploaded file from local storage 
-const uploadedFileJSON = sessionStorage.getItem('uploadedFile');
+let renderer, scene, camera, controls, helper;
+const uploadedFileJSON = sessionStorage.getItem("uploadedFile");
 const jsonData = JSON.parse(uploadedFileJSON);
+init();
+animate();
 
-const container = document.getElementById('content');
+function init() {
+  // renderer
+  const container = document.getElementById("content");
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.autoClear = false;
+  container.appendChild(renderer.domElement);
 
-const scene = new THREE.Scene();
-const scene2 = new THREE.Scene();
+  // scene
+  scene = new THREE.Scene();
+  // camera
+  camera = new THREE.PerspectiveCamera(
+    40,
+    container.clientWidth / container.clientHeight,
+    1,
+    10000
+  );
+  camera.position.set(20, 20, 20);
 
-const camera = new THREE.PerspectiveCamera( 70, container.clientWidth / container.clientHeight, 0.1, 1000 );
+  // controls
+  controls = new OrbitControls(camera, renderer.domElement);
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-const renderer2 = new THREE.WebGLRenderer({
-	alpha: true,
-    antialias: true 
-});
+  // Background color
+  scene.background = new THREE.Color(0xc8ccc9);
 
+  //light settings
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+  const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight2.position.z = -2;
+  scene.add(directionalLight2);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+  scene.add(ambientLight);
 
-// Set the background color
-renderer.setClearColor(0x000000); 
-// draw figure
-drawFigure(jsonData, scene); 
-// draw constraints 
-drawConstraints(jsonData, scene);
-// draw forces 
-drawForces(jsonData, scene);
-// add axises 
-const textGroup = addAxises(scene2);
+  // axes
+  //   scene.add(new THREE.AxesHelper(20));
 
-//allow user to rotate the figure 
-const controls = new OrbitControls(camera, renderer.domElement);
-const controls2 = new OrbitControls(camera, renderer2.domElement);
+  drawFigure(jsonData, scene);
+  // draw constraints
+  drawConstraints(jsonData, scene);
+  // draw forces
+  drawForces(jsonData, scene);
 
-
-renderer.setSize( container.clientWidth, container.clientHeight );
-
-document.getElementById('results').appendChild( renderer.domElement);
-document.getElementById('axises').appendChild( renderer2.domElement);
-
-
-//light settings 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
-directionalLight.position.z = 2
-scene.add(directionalLight)
-const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1)
-directionalLight2.position.z = -2
-scene.add(directionalLight2)
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
-scene.add(ambientLight);
-
-// Set camera position and look at the center of the scene
-camera.position.set(15, 15, 15);
-camera.lookAt(0, 0, 0);
+  // helper
+  helper = new ViewHelper(camera, renderer);
+  helper.setControls(controls);
+}
 
 function animate() {
-   // Iterate through all the children of the textGroup
-   textGroup.children.forEach((textLabel) => {
-    // Create a rotation matrix that aligns the text label with the camera direction
-    const rotationMatrix = new THREE.Matrix4().lookAt(camera.position, textLabel.position, camera.up);
-    // Apply the rotation to the text label
-    textLabel.quaternion.setFromRotationMatrix(rotationMatrix);
-});
-    requestAnimationFrame( animate );
-	renderer.render( scene, camera );
-    renderer2.render( scene2, camera );
+  requestAnimationFrame(animate);
+
+  renderer.clear();
+
+  renderer.render(scene, camera);
+
+  helper.render(renderer);
 }
-animate();
